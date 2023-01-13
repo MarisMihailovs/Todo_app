419af709024c0bd24ca09365a33cc400258db2e4
MarisMihailovs <marismihailovs@gmail.com>
2023-01-13 17:23:44 +0200

    drag functions

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
 const addItems = document.querySelectorAll('.add-item');
 // Item Lists
-const itemLists = document.querySelectorAll('.drag-item-list');
+const listColumn = document.querySelectorAll('.drag-item-list');
 const backlogList = document.getElementById('backlog-list');
 const progressList = document.getElementById('progress-list');
 const completeList = document.getElementById('complete-list');
 const onHoldList = document.getElementById('on-hold-list');
 
 // Items
-
+let updatedOnLoad = false;
 
 // Initialize Arrays
 let backlogListArray = [];
@@ -20,7 +20,8 @@ let onHoldListArray = [];
 let listArrays = [];
 
 // Drag Functionality
-
+let draggedItem;
+let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -33,11 +34,10 @@ function getSavedColumns() {
     backlogListArray = ['do nothing', 'drink coffee'];
     progressListArray = ['Work on projects', 'Listen to music'];
     completeListArray = ['code', 'Getting stuff done'];
-    onHoldListArray = ['walk the dog'];
+    onHoldListArray = ['walk the dog', 'take trash out'];
   }
 }
-getSavedColumns();
-updateSavedColumns();
+
 
 // Set localStorage Arrays
 function updateSavedColumns() {
@@ -56,30 +56,90 @@ function updateSavedColumns() {
 
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
-  console.log('columnEl:', columnEl);
-  console.log('column:', column);
-  console.log('item:', item);
-  console.log('index:', index);
+  // console.log('columnEl:', columnEl);
+  // console.log('column:', column);
+  // console.log('item:', item);
+  // console.log('index:', index);
   // List Item
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
+  listEl.textContent = item;
+  listEl.draggable = true;
+  listEl.setAttribute('ondragstart', 'drag(event)');
+  // append
+  columnEl.appendChild(listEl);
 
 }
 
 // Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
 function updateDOM() {
   // Check localStorage once
-
+  if (!updatedOnLoad) {
+    getSavedColumns();
+  }
   // Backlog Column
-
+  backlogList.textContent = '';
+  backlogListArray.forEach((backlogItem, index) => {
+    createItemEl(backlogList, 0, backlogItem, index);
+  });
   // Progress Column
-
+  progressList.textContent = '';
+  progressListArray.forEach((progressItem, index) => {
+    createItemEl(progressList, 0, progressItem, index);
+  });
   // Complete Column
-
+  completeList.textContent = '';
+  completeListArray.forEach((completeItem, index) => {
+    createItemEl(completeList, 0, completeItem, index);
+  });
   // On Hold Column
-
+  onHoldList.textContent = '';
+  onHoldListArray.forEach((onHoldItem, index) => {
+    createItemEl(onHoldList, 0, onHoldItem, index);
+  });
   // Run getSavedColumns only once, Update Local Storage
 
 
 }
 
+// ondrag
+function drag(e) {
+  draggedItem = e.target;
+  console.log(draggedItem);
+}
+
+// column allows for item to drop
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+// allow drop 
+function dragEnter(column) {
+  listColumn[column].classList.add('over');
+  currentColumn = column;
+}
+
+function dragleave() {
+  this.classList.remove('over');
+}
+
+// dropping item in column
+function drop(e) {
+  e.preventDefault();
+  // remove background color/padding
+  listColumn.forEach((column) => {
+    column.classList.remove('over');
+  });
+  // add item to column
+  const parent = listColumn[currentColumn];
+  parent.appendChild(draggedItem);
+
+}
+
+listColumn[0].addEventListener("dragleave", dragleave)
+listColumn[1].addEventListener("dragleave", dragleave)
+listColumn[2].addEventListener("dragleave", dragleave)
+listColumn[3].addEventListener("dragleave", dragleave)
+// onload
+
+updateDOM();
